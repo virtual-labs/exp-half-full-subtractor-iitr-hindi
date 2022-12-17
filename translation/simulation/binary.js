a87e75b4e3b3ab525acf296f792d365f62569b3b
vandan mujadia <vmujadia@gmail.com>
2022-12-17 15:24:13 +0530

    added Hindi Translation

---
+++ translation/simulation/binary.js
@@ -0,0 +1,118 @@
+function checkmyinput () {
+    // Get inputfield
+    var inputfield = document.getElementById("A");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
+
+function checkmyinputB () {
+    // Get inputfield
+    var inputfield = document.getElementById("B");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
+function checkmyinputC () {
+    // Get inputfield
+    var inputfield = document.getElementById("C");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
+
+
+function checkmyinputZ () {
+    // Get inputfield
+    var inputfield = document.getElementById("Z");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
+
+function checkmyinputCin () {
+    // Get inputfield
+    var inputfield = document.getElementById("Borrow");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
+
+function checkmyinputP () {
+    // Get inputfield
+    var inputfield = document.getElementById("Sub");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
+
+function checkmyinputD () {
+    // Get inputfield
+    var inputfield = document.getElementById("D");
+
+   
+    // Get value from inputfield
+    var inputval = inputfield.value;
+    
+    // Remove non numeric input
+    var numeric = inputval.replace(/[^0-9]+/,"");
+    
+    // Check if input is numeric and even, if not empty field
+    if (numeric.length != inputval.length || numeric != 0 &&  numeric != 1 ) {
+        inputfield.value = '';
+    }
+}
